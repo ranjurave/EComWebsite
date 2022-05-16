bb40d3e9ad6819c4cf73c35ef40b123a56f09328
ranjurave <ranjurave@gmail.com>
2022-05-16 23:15:29 +1200

    until video time3:48, works fine

--- EComWebApp/market/__init__.py
+++ EComWebApp/market/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = '2fa5a3dca2c59a3177137ff2'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
 from market import routes
