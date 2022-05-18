483026a5cafe24004a883df7741f9613d8a9dbeb
ranjurave <ranjurave@gmail.com>
2022-05-18 15:38:49 +1200

    to work from 4:17

--- EComWebApp/market/__init__.py
+++ EComWebApp/market/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = '2fa5a3dca2c59a3177137ff2'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
 
 from market import routes
--- EComWebApp/market/routes.py
+++ EComWebApp/market/routes.py
@@ -1,8 +1,9 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForms
 from market import db
+from flask_login import login_user
 
 @app.route("/")
 @app.route("/home")
@@ -26,3 +27,16 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f"There was error creating user: {err_msg}", category='danger')
     return render_template('register.html', form=form)
+
+@app.route('/login',methods=['GET', 'POST'])
+def login_page():
+    form = LoginForms()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as :{attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('User name and password dont match. Please try again', category='danger')
+    return render_template('login.html', form=form)
