4a9b72a0ef5e36df2f0fbfa3796f2b27344a3c9a
ranjurave <ranjurave@gmail.com>
2022-05-18 22:50:40 +1200

    done upto 4:50 in video

--- EComWebApp/market/routes.py
+++ EComWebApp/market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForms
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route("/")
 @app.route("/home")
@@ -11,6 +11,7 @@ def home_page():
     return render_template("home.html")
 
 @app.route("/market")
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
@@ -22,6 +23,8 @@ def register_page():
          user_to_create = User(username=form.username.data, email_address = form.email_address.data, password= form.password1.data)
          db.session.add(user_to_create)
          db.session.commit()
+         login_user(user_to_create)
+         flash(f'Account created successfully. You are logged in as {user_to_create.username}', category='success')
          return redirect(url_for('market_page'))
     if form.errors != {}: #if there re no errors from validation
         for err_msg in form.errors.values():
@@ -40,3 +43,9 @@ def login_page():
         else:
             flash('User name and password dont match. Please try again', category='danger')
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have been logged out', category='info')
+    return redirect(url_for("home_page"))
